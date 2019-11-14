284c5f1b38062a72762942c694b924aaf772cd62
khangdm <minhkhang231199@gmail.com>
2019-11-14 07:36:46 +0700

    "no thing change"

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,6 @@
 ﻿import { Routes, RouterModule } from '@angular/router';
 
-import { HomeComponent } from './home';
+import { HomeComponent } from './home'; 
 import { LoginComponent } from './login';
 import { AuthGuard } from './_helpers';
 import { MainComponent } from './main/main.component';
